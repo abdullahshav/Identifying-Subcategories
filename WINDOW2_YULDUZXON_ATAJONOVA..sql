ce924295989fc087a8a83d913bafad678f57a46a
abdullahshav <144883374+abdullahshav@users.noreply.github.com>
2024-01-09 16:08:11 +0500

    Update WINDOW2_YULDUZXON_ATAJONOVA..sql

--- WINDOW2_YULDUZXON_ATAJONOVA..sql
+++ WINDOW2_YULDUZXON_ATAJONOVA..sql
@@ -1,28 +1,14 @@
-WITH SubcategorySales AS (
-    SELECT
-        prod_subcategory,
-        sales_year,
-        SUM(sales) AS total_sales
-    FROM
-        your_sales_table
-    WHERE
-        sales_year IN (1998, 1999, 2000, 2001)
-    GROUP BY
-        prod_subcategory, sales_year
-),
-PreviousYearSales AS (
-    SELECT
-        prod_subcategory,
-        sales_year,
-        LAG(total_sales) OVER (PARTITION BY prod_subcategory ORDER BY sales_year) AS previous_year_sales
-    FROM
-        SubcategorySales
+WITH subcategory_sales AS (
+    SELECT prod_subcategory,
+        t.calendar_year AS order_year,
+        SUM(s.amount_sold) AS total_sales,
+        LAG(SUM(s.amount_sold)) OVER (PARTITION BY prod_subcategory ORDER BY calendar_year) AS prev_year_sales
+    FROM sh.sales s
+	JOIN sh.products p USING(prod_id)
+	JOIN sh.times t USING(time_id)
+    WHERE t.calendar_year BETWEEN 1998 AND 2001
+    GROUP BY prod_subcategory, t.calendar_year
 )
-SELECT DISTINCT
-    s.prod_subcategory
-FROM
-    SubcategorySales s
-JOIN
-    PreviousYearSales p ON s.prod_subcategory = p.prod_subcategory AND s.sales_year = p.sales_year
-WHERE
-    s.total_sales > COALESCE(p.previous_year_sales, 0);
+SELECT DISTINCT prod_subcategory, order_year, total_sales, prev_year_sales
+FROM subcategory_sales
+WHERE total_sales > prev_year_sales;
